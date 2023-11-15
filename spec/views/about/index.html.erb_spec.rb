c4ecdd0dd05b3aefc94f140a37ab713b54ec071e
Kennedy Gosnell <kennedygosnell@outlook.com>
2023-11-15 03:25:20 -0800

    add about page, add specs/ /helpers /requests /views + routing to link about page

---
+++ spec/views/about/index.html.erb_spec.rb
@@ -0,0 +1,11 @@
+<div class="container mt-5">
+  <div class="row">
+    <div class="col-lg-8 offset-lg-2">
+      <h1 class="display-4 text-center mb-4">About Jungle</h1>
+      <p class="lead text-center">
+        Welcome to Jungle, your ultimate destination for all things green and natural! We are passionate about
+        plants and believe in bringing the beauty of nature into your life and home.
+      </p>
+    </div>
+  </div>
+</div>
